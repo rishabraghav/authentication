923cda3b5a58ca6e72dccaf2c24797bb5c2d9e7b
Rishabh Raghav <rishabhraghav2012@gmail.com>
2022-08-18 13:44:59 +0530

    LEVEL 3 - Encryption(Added environment variables)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
-// const ejs = require("ejs");
+const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const encryption = require('mongoose-encryption');
@@ -18,8 +19,8 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-var secret = "ThisIsSomethingThatYouDontKnow";
-userSchema.plugin(encryption, { secret: secret, encryptedFields: ['password'] });
+
+userSchema.plugin(encryption, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -61,6 +62,8 @@ app.post("/login", (req, res) => {
                 } else {
                     res.render("wrongPass");
                 }
+            } else {
+                console.log("not registered");
             }
         }
     });
